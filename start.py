511a87676dc3c7eb23263b38b151aec235db6104
pawan00126 <pawanbhati2002@gmail.com>
2026-01-20 00:34:05 +0530

    20 Jan 2026 - strings

--- start.py
+++ start.py
@@ -1,27 +1,43 @@
-print('hello by pawan :) ')
+# print('hello by pawan :) ')
 
-x = 'Hello'
+# x = 'Hello'
 
-# y = 'Hii'
+# # y = 'Hii'
 
-z = 'Yo'
+# z = 'Yo'
 
-# print(x,y,z)
+# # print(x,y,z)
 
-# print(x+y+z)
+# # print(x+y+z)
 
-A = 'Hello Yo'
+# A = 'Hello Yo'
 
-def hii():
-    global y
-    y = 'I am inside the function as global var'
+# def hii():
+#     global y
+#     y = 'I am inside the function as global var'
 
-hii()
+# hii()
 
-print(y)
+# print(y)
 
-# print('first') print('second') # Error 
-print('first'); print('second') # prints the second on new line
-print('first', end=' '); print('second') # prints the second on same line
-print('first', end=' ')
-print('second') # prints the second on same line
+# # print('first') print('second') # Error 
+# print('first'); print('second') # prints the second on new line
+# print('first', end=' '); print('second') # prints the second on same line
+# print('first', end=' ')
+# print('second') # prints the second on same line
+
+
+# a = 'Hello World'
+
+# print(a[1])
+
+x = 'Hello_World'
+ 
+#  x[start_idx: end_idx: step] # end_idx is exclusive
+ 
+print(x[1:3]) #
+print(x[1::2]) #
+
+
+d = 'Hello, I am Pawan'
+print(d.split(',')) # ['Hello', 'I am Pawan']
