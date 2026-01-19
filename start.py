68c71bdf9f8bd4120b069d87a1f1967d158322e4
pawan00126 <pawanbhati2002@gmail.com>
2026-01-20 00:04:50 +0530

    comments

--- start.py
+++ start.py
@@ -1,1 +1,27 @@
 print('hello by pawan :) ')
+
+x = 'Hello'
+
+# y = 'Hii'
+
+z = 'Yo'
+
+# print(x,y,z)
+
+# print(x+y+z)
+
+A = 'Hello Yo'
+
+def hii():
+    global y
+    y = 'I am inside the function as global var'
+
+hii()
+
+print(y)
+
+# print('first') print('second') # Error 
+print('first'); print('second') # prints the second on new line
+print('first', end=' '); print('second') # prints the second on same line
+print('first', end=' ')
+print('second') # prints the second on same line
