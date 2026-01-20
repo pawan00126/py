b247a0b7cf89ce9049bba2ce3c6ea397e15e5477
pawan00126 <pawanbhati2002@gmail.com>
2026-01-20 23:32:41 +0530

    feat operators

---
+++ start_20_jan_2026.py
@@ -0,0 +1,34 @@
+# print('Hello, From Pawan :)')
+
+
+# x, y = 'Hello', 'Pawan', 'Bhati' # ValueError: too many values to unpack (expected 2)
+
+# print(x)
+# print(y)
+
+# Operators 
+
+print(2**3)
+
+
+
+x = 5
+
+print(1 < x < 10)
+
+print(1 < x and x < 10)
+
+
+x = ["apple","banana"]  
+y = ["apple","banana"]  
+z = x  
+  
+print(x is z)  
+print(x is y)  
+print(x == y)
+
+
+x = ["apple", "banana"]
+y = ["apple", "banana"]
+
+print(x is not y)
